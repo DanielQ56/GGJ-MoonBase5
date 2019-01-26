7a31ee9d93b7aee75e80e306a26eb910989ab748
Matthew Fernandez <mattfer98@gmail.com>
2019-01-25 22:47:36 -0800

    Made "Player" folder & created playerFamiliarity script

---
+++ Terra Luna/Assets/Player/playerFamiliarity.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFamiliarity : MonoBehaviour
+{
+    // Referring to familiarity as "points" in this script
+
+    public float startingPoints = 0;
+    public float maxPoints = 100;
+    public float minPoints = 0;
+
+    float currentPoints;
+
+    void Start()
+    {
+        currentPoints = startingPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float currentFamiliarity()
+    {
+        return currentPoints;
+    }
+
+    public void addFamiliarity(float pointsToAdd)
+    {
+        if (pointsToAdd > (maxPoints - currentPoints)){
+            currentPoints = maxPoints;
+        } else {
+            currentPoints += pointsToAdd;
+        }
+    }
+
+    public void removeFamiliarity(float pointsToRemove)
+    {
+        if (pointsToRemove > (currentPoints - minPoints)){
+            currentPoints = minPoints;
+        } else {
+            currentPoints -= minPoints;
+        }
+    }
+}
