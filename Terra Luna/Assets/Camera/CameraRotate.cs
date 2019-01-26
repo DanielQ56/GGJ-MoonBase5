3e1cec1ebb184031696480b64c64de6e7005e43a
Danny QI <drqi@uci.edu>
2019-01-26 02:04:03 -0800

    Created simple player movement script & basic camera rotation

---
+++ Terra Luna/Assets/Camera/CameraRotate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField] float rotateDegree;
+    [SerializeField] GameObject player;
+    [SerializeField] float speed;
+
+
+    bool stuckAgainstWall = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            CameraRotatation();
+    }
+
+    /*private void LateUpdate()
+    {
+        if (!inDistance())
+        {
+            transform.LookAt(player.transform, Vector3.up);
+            KeepDistance();
+        }
+    }*/
+    void CameraRotatation()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, Input.GetAxisRaw("CameraRotate") * rotateDegree);
+    }
+
+    /*bool inDistance()
+    {
+        return Mathf.Sqrt(Mathf.Pow((transform.position.x - player.transform.position.x), 2) + Mathf.Pow((transform.position.z - player.transform.position.z), 2)) < 22f;
+
+
+    }
+
+    void KeepDistance()
+    {
+        float interpolation = speed * Time.deltaTime ;
+        float x_pos = Mathf.Lerp(transform.position.x, player.transform.position.x, interpolation); 
+        float z_pos = Mathf.Lerp(transform.position.z, player.transform.position.z, interpolation);
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(x_pos, transform.position.y, z_pos), 0.2f);
+    }*/
+}
---
+++ Terra Luna/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float rotateDegree;
+    [SerializeField] float moveSpeed;
+
+    
+    Rigidbody m_RigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    
+    }
+
+    void Move()
+    {
+        m_RigidBody.velocity = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+    }
+
+    void Rotate()
+    {
+        transform.eulerAngles += new Vector3(0, Input.GetAxisRaw("Horizontal") * rotateDegree, 0);
+    }
+}
