7d05776b038a10b1d3f6e40ef843545ab057fe2a
Matthew Fernandez <mattfer98@gmail.com>
2019-01-26 14:22:14 -0800

    Made familiarity tree change state with player's overall familiarity

---
+++ Terra Luna/Assets/treeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeController : MonoBehaviour
+{
+    // First stage is the minimum possible familiarity
+    public int SecondStageMinimum = 25;
+    public int ThirdStageMinimum = 50;
+    public int FinalStageMinimum = 75;
+
+    // "stageObject" means the different GameObjects containing different meshes/etc.
+    // for the different stages of the tree
+    public GameObject initialStageObject;
+    public GameObject secondStageObject;
+    public GameObject thirdStageObject;
+    public GameObject finalStageObject;
+
+    GameObject player;
+    GameObject currentStageObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in transform){
+            child.gameObject.SetActive(false);
+        }
+        player = GameObject.FindWithTag("Player");
+        currentStageObject = initialStageObject;
+        currentStageObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTreeState();
+    }
+
+    void updateTreeState()
+    {
+        float current = player.GetComponent<playerFamiliarity>().currentFamiliarity();
+
+        if (current > FinalStageMinimum){
+            changeTreeStage(finalStageObject);
+        } else if (current > ThirdStageMinimum){
+            changeTreeStage(thirdStageObject);
+        } else if (current > SecondStageMinimum){
+            changeTreeStage(secondStageObject);
+        } else {
+            changeTreeStage(initialStageObject);
+        }
+        
+    }
+
+    void changeTreeStage(GameObject newStageObject){
+        if (newStageObject != currentStageObject){
+            currentStageObject.SetActive(false);
+            currentStageObject = newStageObject;
+            currentStageObject.SetActive(true);
+        }
+    }
+}
