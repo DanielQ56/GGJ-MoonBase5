467cdaf31f6db19bd680419151808a9eeab76136
Annie Xu <axu.1492@gmail.com>
2019-01-26 21:59:31 -0800

    finished menu scene

---
+++ Terra Luna/Assets/Scenes/MenuCredits/RotateObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjects : MonoBehaviour
+{
+	public GameObject[] rotatingObjects;
+	public float rotateSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < rotatingObjects.Length; i++) {
+			rotatingObjects[i].transform.RotateAround(transform.position, Vector3.up, rotateSpeed*Time.deltaTime);
+	    } 
+    }
+}
---
+++ Terra Luna/Assets/Scenes/MenuCredits/exitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitScript : MonoBehaviour
+{
+	
+	public void ExitGame() {
+		Application.Quit();
+	}
+	
+}
