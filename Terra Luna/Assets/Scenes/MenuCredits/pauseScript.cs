2041bd204ce382b278b6b18bd9dbf1490e21140d
Annie Xu <axu.1492@gmail.com>
2019-01-27 08:41:30 -0800

    pause, menu, credits

---
+++ Terra Luna/Assets/Scenes/MenuCredits/pauseScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseScript : MonoBehaviour
+{
+	GameObject[] pauseObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hideOnPause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) {
+			if(Time.timeScale == 0) {
+				Time.timeScale = 1;
+				hideOnPause();
+			} else {
+				Time.timeScale = 0;
+				showOnPause();
+			}
+		}
+    }
+	
+	public void hideOnPause() {
+		foreach(GameObject g in pauseObjects) {
+			g.SetActive(false);
+		}
+	}
+	
+	public void showOnPause() {
+		foreach(GameObject g in pauseObjects) {
+			g.SetActive(true);
+		}
+	}
+}
---
+++ Terra Luna/Assets/Scenes/MenuCredits/resumeScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resumeScript : MonoBehaviour
+{
+    public void ResumeGame() {
+		Time.timeScale = 1;
+	}
+}
