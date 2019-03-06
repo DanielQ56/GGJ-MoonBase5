f00e80f9f1c8840dd20ccad6bc1a38ff58319c11
Danny QI <drqi@uci.edu>
2019-03-06 13:17:50 -0800

    Cleaned up some stuff

---
+++ Terra Luna/Assets/Scenes/MenuCredits/ButtonFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!panel.activeInHierarchy)
+            {
+                panel.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Resume();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
